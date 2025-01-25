df2b43dc986962b539c7adbc3a33b46dadeeeae1
danielbarlev2 <danielbl2010@gmail.com>
2025-01-25 18:14:31 +0200

    run now supports --scene-editor

--- run.sh
+++ run.sh
@@ -4,14 +4,15 @@
 function usage() {
     echo "Usage: $0 [OPTIONS]"
     echo "Options:"
-    echo "  --generate-traces    Run the generate_traces function."
-    echo "  --motion-control     Run the finetuned_motion_control function."
-    echo "  --no-clear           Skip clearing the screen at the beginning."
-    echo "  --no-clear-pycache   Skip removing __pycache__ directories at the end."
-    echo "  --help               Display this help message."
+    echo "  --generate-traces       Run the generate_traces function."
+    echo "  --motion-control        Run the finetuned_motion_control function."
+    echo "  --run-scene-editor      Run the scene_editor script."
+    echo "  --no-clear              Skip clearing the screen at the beginning."
+    echo "  --no-clear-pycache      Skip removing __pycache__ directories at the end."
+    echo "  --help                  Display this help message."
     echo
     echo "Example:"
-    echo "  $0 --generate-traces --motion-control"
+    echo "  $0 --generate-traces --motion-control --run-scene-editor"
     exit 1
 }
 
@@ -28,6 +29,10 @@ for arg in "$@"; do
             ;;
         --motion-control)
             MOTION_CONTROL=true
+            shift
+            ;;
+        --run-scene-editor)
+            RUN_SCENE_EDITOR=true
             shift
             ;;
         --no-clear)
@@ -66,6 +71,19 @@ if [ "$MOTION_CONTROL" = true ]; then
     /home/ML_courses/03683533_2024/anton_kfir_daniel/conda/miniconda3/envs/priorMDM_env/bin/python -m sample.finetuned_motion_control \
         --model_path save/root_horizontal_finetuned/model000280000.pt \
         --text_condition "a person is raising hands"
+fi
+
+# Run the scene_editor script if requested
+if [ "$RUN_SCENE_EDITOR" = true ]; then
+    echo "Running scene_editor..."
+    /home/ML_courses/03683533_2024/anton_kfir_daniel/conda/miniconda3/envs/trace_2/bin/python scripts/scene_editor.py \
+        --results_root_dir ./out/orca_mixed_out \
+        --num_scenes_per_batch 1 \
+        --policy_ckpt_dir ./ckpt/trace/orca_mixed \
+        --policy_ckpt_key iter40000 \
+        --eval_class Diffuser \
+        --render_img \
+        --config_file ./configs/eval/orca/target_pos.json
 fi
 
 # Remove __pycache__ directories at the end if not skipped
